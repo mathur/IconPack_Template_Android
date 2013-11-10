752f051dc33bbec44537cbd2d6948aced8c0f849
Rohan Mathur <rohanmathur34@gmail.com>
2013-11-09 21:51:39 -0500

    Initial OSP push
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

---
+++ src/com/yourname/themename/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yourname.themename;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
